2b07069d12a421c0fcc6374ec1b019478a98cf43
Palmer Dabbelt <palmer@dabbelt.com>
2014-12-05 11:07:55 -0800

    Add a test case!
    This is a regression test for
    
      6f7b92f Work around some Krste's mail client bugs

---
+++ test/mhng-show/harness_start.bash
@@ -0,0 +1,63 @@
+set -ex
+
+tempdir=`mktemp -d -t mhng-test.XXXXXXXXXX`
+trap "rm -rf $tempdir" EXIT
+cd $tempdir
+
+mkdir maildir
+export MHNG_MAILDIR="$(readlink -f maildir)"
+export MHNG_NO_DAEMON="true"
+
+cat >init.sql <<EOF
+CREATE TABLE MH__current (folder STRING NOT NULL CHECK(folder != ''),
+                          seq INTEGER NOT NULL CHECK(seq > 0),
+                          cur INTEGER CHECK(cur > 0 AND cur <= 1),
+                          uid_validity INTEGER CHECK(uid_validity >= 0),
+                          UNIQUE(folder),
+                          UNIQUE(cur)
+       );
+INSERT INTO MH__current (folder, seq, cur) VALUES ("inbox", 1, 1);
+INSERT INTO MH__current (folder, seq) VALUES ("drafts", 1);
+INSERT INTO MH__current (folder, seq) VALUES ("rss", 1);
+INSERT INTO MH__current (folder, seq) VALUES ("promo", 1);
+INSERT INTO MH__current (folder, seq) VALUES ("lists", 1);
+INSERT INTO MH__current (folder, seq) VALUES ("sent", 1);
+INSERT INTO MH__current (folder, seq) VALUES ("trash", 1);
+
+CREATE TABLE MH__messages (uid INTEGER PRIMARY KEY CHECK(uid > 0),
+                           seq INTEGER NOT NULL CHECK(seq > 0),
+                           folder STRING NOT NULL CHECK(folder != ''),
+                           unread INTEGER NOT NULL CHECK(unread >= 0 AND unread <= 2),
+                           subject STRING NOT NULL,
+                           date INTEGER NOT NULL CHECK(date > 0),
+                           fadr STRING NOT NULL,
+                           tadr STRING NOT NULL,
+                           UNIQUE(uid),
+                           UNIQUE(folder, seq)
+       );
+
+CREATE TABLE MH__nextid (single INTEGER NOT NULL CHECK(single > 0 AND single < 2),
+                         uid INTEGER NOT NULL,
+                         UNIQUE(uid),
+                         UNIQUE(single)
+       );
+INSERT INTO MH__nextid (single, uid) VALUES (1, 2);
+
+CREATE TABLE IMAP__messages (folder STRING NOT NULL,
+                             uid INTEGER NOT NULL,
+                             mhid INTEGER NOT NULL,
+                             purge INTEGER CHECK(purge >= 0 AND purge <= 1),
+                             UNIQUE(folder, uid),
+                             UNIQUE(mhid)
+       );
+EOF
+sqlite3 $MHNG_MAILDIR/metadata.sqlite3 < init.sql
+
+mkdir -p $MHNG_MAILDIR/tmp
+mkdir -p $MHNG_MAILDIR/mail/inbox
+mkdir -p $MHNG_MAILDIR/mail/drafts
+mkdir -p $MHNG_MAILDIR/mail/rss
+mkdir -p $MHNG_MAILDIR/mail/promo
+mkdir -p $MHNG_MAILDIR/mail/lists
+mkdir -p $MHNG_MAILDIR/mail/sent
+mkdir -p $MHNG_MAILDIR/mail/trash
---
+++ test/mhng-show/utf8-in-ascii.bash
@@ -0,0 +1,76 @@
+#include "harness_start.bash"
+
+ARGS="inbox 1"
+
+sqlite3 $MHNG_MAILDIR/metadata.sqlite3 <<EOF
+INSERT INTO MH__messages (uid, seq, folder, unread, subject, date, fadr, tadr) VALUES (2, 1, "inbox", 0, "test", 1, "from@example.com", "to@example.com");
+EOF
+
+cat >$MHNG_MAILDIR/mail/inbox/2 <<"EOF"
+Message-ID: <REDACTED0@example.com>
+Date: Tue, 2 Dec 2014 23:15:26 -0800
+From: REDACTED1@example.com
+To: REDACTED2@example.com
+Subject: Soft Machines "VISC"?
+In-Reply-To: <REDACTED3@example.com>
+References: <REDACTED4@example.com>
+X-Mailer: VM 8.2.0b under 24.3.1 (x86_64-apple-darwin12.4.0)
+Mime-Version: 1.0
+Content-Type: multipart/mixed;
+	boundary="++----------20141202231523-1330795279022344039----------++"
+Content-Transfer-Encoding: 8bit
+Cc: REDACTED5@example.com
+X-BeenThere: REDACTED5@example.com
+X-Mailman-Version: 2.1.9
+Precedence: list
+
+This is a multi-part message in MIME format.
+--++----------20141202231523-1330795279022344039----------++
+Content-Type: text/plain; charset=us-ascii
+Content-Transfer-Encoding: 8bit
+
+
+Here's the microprocessor report article from their website.
+
+It looks like an implementation of the ILDP idea by Jim Smith et al.
+
+Krste
+
+>>>>> On Tue, 2 Dec 2014 21:50:25 -0800, REDACTED5 <REDACTED5@example.com> said:
+
+| Has anyone heard anything about Soft Machines and their “virtual
+| hardware threads"?  I stumbled across it and I can’t tell if it is
+| actually interesting or just some weird Transmeta rehash.  -Ben
+
+--++----------20141202231523-1330795279022344039----------++
+Content-Type: application/pdf; name="MPR-11303.pdf"
+Content-Transfer-Encoding: base64
+
+JVBERi0xLjYNJeLjz9MNCjE3MiAwIG9iag08PC9GaWx0ZXIvRmxhdGVEZWNvZGUvRmlyc3QgNTcx
+
+--++----------20141202231523-1330795279022344039----------++--
+EOF
+
+cat >out.gold <<"EOF"
+From:    REDACTED1@example.com
+To:      REDACTED2@example.com
+CC:      REDACTED5@example.com
+Subject: Soft Machines "VISC"?
+Date:    Tue, 02 Dec 2014 23:15:26 PST (-0800)
+
+
+Here's the microprocessor report article from their website.
+
+It looks like an implementation of the ILDP idea by Jim Smith et al.
+
+Krste
+
+>>>>> On Tue, 2 Dec 2014 21:50:25 -0800, REDACTED5 <REDACTED5@example.com> said:
+
+| Has anyone heard anything about Soft Machines and their “virtual
+| hardware threads"?  I stumbled across it and I can’t tell if it is
+| actually interesting or just some weird Transmeta rehash.  -Ben
+
+EOF
+
+#include "harness_end.bash"
