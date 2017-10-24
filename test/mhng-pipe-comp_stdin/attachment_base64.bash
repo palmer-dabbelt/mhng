77dd59470c9712209f2cad51f39e9791d0384dea
Palmer Dabbelt <palmer@dabbelt.com>
2017-10-24 09:36:41 -0700

    Actually run the pipe-comp_stdin tests
    This also fixes the bug I just introduced :)

--- test/mhng-pipe-comp_stdin/attachment_base64.bash
+++ test/mhng-pipe-comp_stdin/attachment_base64.bash
@@ -25,6 +25,28 @@ aGVsbG8K
 
 EOF
 
-cp in.test out.test
+cat >out.gold <<"EOF"
+Message-ID: <REDACTED@email.amazonses.com>
+Subject: Shipping Label for your Trade-In (TRN-REDACTED4)
+MIME-Version: 1.0
+Content-Type: multipart/alternative;
+	boundary="----=_Part_REDATCED3"
+Date: Sat, 10 Dec 2016 20:03:44 +0000
+To: REDATCED2@example.com
+
+------=_Part_REDATCED3
+Content-Type: text/plain; charset=utf-8
+Content-Transfer-Encoding: 7bit
+
+Look, content!
+------=_Part_REDATCED3
+Content-Type: text/html; charset=utf-8
+Content-Transfer-Encoding: base64
+
+aGVsbG8K
+------=_Part_REDATCED3--
+
+EOF
+
 
 #include "harness_end.bash"
